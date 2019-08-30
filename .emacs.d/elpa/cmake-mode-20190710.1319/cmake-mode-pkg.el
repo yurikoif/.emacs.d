5e8ea9d1759c87c9d45f9ad8bc0492c1e1991ba2
richard <yurikoif@gmail.com>
2019-08-29 22:05:49 -0400

    add cmake mode

---
+++ .emacs.d/elpa/cmake-mode-20190710.1319/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "3ec986ce8e8df269eb6b6b9f37e12b02194168fd")
