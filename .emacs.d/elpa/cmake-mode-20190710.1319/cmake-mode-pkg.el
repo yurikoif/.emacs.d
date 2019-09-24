b9d0ccf3acf57e3def9677dec82b513eefb2864a
richard <yurikoif@gmail.com>
2019-09-24 18:04:23 -0400

    1

--- .emacs.d/elpa/cmake-mode-20190710.1319/cmake-mode-pkg.el
+++ .emacs.d/elpa/cmake-mode-20190710.1319/cmake-mode-pkg.el
@@ -1,2 +1,2 @@
 ;;; -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "3ec986ce8e8df269eb6b6b9f37e12b02194168fd")
+(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "dcc117b9446cfc3d6c8bf191545aac8d1519abca")
