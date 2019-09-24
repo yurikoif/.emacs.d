d345809ec581b1c5fdb16d8aa074e53ab55e60b5
richard <yurikoif@gmail.com>
2019-09-24 16:26:01 -0400

    add ivy counsel swiper

---
+++ .emacs.d/elpa/org-starter-20190907.1859/org-starter-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-starter" "20190907.1859" "A basic configuration framework for org mode" '((emacs "25.1") (dash "2.12") (dash-functional "1.2.0")) :commit "ca166579a35dc671ad8f59dbc6dcf2a1eda94a20" :authors '(("Akira Komamura" . "akira.komamura@gmail.com")) :maintainer '("Akira Komamura" . "akira.komamura@gmail.com") :url "https://github.com/akirak/org-starter")
---
+++ .emacs.d/elpa/org-starter-swiper-20190817.1823/org-starter-swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-starter-swiper" "20190817.1823" "Swiper for org-starter" '((emacs "25.1") (swiper "0.11") (org-starter "0.2.4")) :commit "ca166579a35dc671ad8f59dbc6dcf2a1eda94a20" :authors '(("Akira Komamura" . "akira.komamura@gmail.com")) :maintainer '("Akira Komamura" . "akira.komamura@gmail.com") :url "https://github.com/akirak/org-starter")
