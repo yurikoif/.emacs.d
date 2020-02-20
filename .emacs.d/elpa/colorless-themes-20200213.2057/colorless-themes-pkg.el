b4c6d8d721ddf6e3df179ec34bd7803d5272343c
yurikoif <yurikoif@gmail.com>
2020-02-20 16:57:05 -0500

    1

--- .emacs.d/elpa/colorless-themes-20200213.2057/colorless-themes-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "colorless-themes" "20200213.2057" "A macro to generate mostly colorless themes" '((emacs "24.1")) :commit "8e539097185704444a5dd5545924de92c11ba767" :keywords '("faces themes" "faces") :authors '(("Thomas Letan" . "contact@thomasletan.fr")) :maintainer '("Thomas Letan" . "contact@thomasletan.fr") :url "https://git.sr.ht/~lthms/colorless-themes.el")
