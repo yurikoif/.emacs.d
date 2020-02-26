774bfb890276ac74f7e31cdb1c72bacb2ac755bf
yurikoif <yurikoif@gmail.com>
2020-02-26 13:56:32 -0500

    1

--- .emacs.d/elpa/swiper-20200122.1153/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20200122.1153" "Isearch with an overview. Oh, man!" '((emacs "24.5") (ivy "0.13.0")) :commit "6a902b5cbd614c5e8e25a6bf9952ca529784db2d" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
