1cd296e2990a6284eca520f251e42a899c08ff1d
yurikoif <yurikoif@gmail.com>
2020-08-31 16:47:54 -0400

    1

---
+++ elpa/lsp-python-ms-20200825.1651/lsp-python-ms-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lsp-python-ms" "20200825.1651" "The lsp-mode client for Microsoft python-language-server" '((emacs "25.1") (lsp-mode "6.0")) :commit "1e2c0affaf957c119af7366525fc688909e7bbad" :keywords '("languages" "tools") :authors '(("Charl Botha")) :maintainer '("Andrew Christianson, Vincent Zhang") :url "https://github.com/emacs-lsp/lsp-python-ms")
