e034cb1ae841b4731909ec88f677d8417eb83356
yurikoif <yurikoif@gmail.com>
2020-09-06 18:40:59 -0400

    1

--- build_emacs.sh
+++ build_emacs.sh
@@ -1,4 +1,4 @@
-git clone --depth=1 https://github.com/emacs-mirror/emacs.git
+git clone --single-branch --branch emacs-27 --depth 1 https://github.com/emacs-mirror/emacs.git
 cd emacs/
 sudo apt install -y autoconf make gcc texinfo libgtk-3-dev libxpm-dev \
      libjpeg-dev libgif-dev libtiff5-dev libgnutls28-dev libncurses5-dev \
