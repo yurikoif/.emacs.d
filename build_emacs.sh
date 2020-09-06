9b98f5842d5339c587bb986498eabd0597efd59b
yurikoif <yurikoif@gmail.com>
2020-09-06 18:38:44 -0400

    1

---
+++ build_emacs.sh
@@ -0,0 +1,14 @@
+git clone --depth=1 https://github.com/emacs-mirror/emacs.git
+cd emacs/
+sudo apt install -y autoconf make gcc texinfo libgtk-3-dev libxpm-dev \
+     libjpeg-dev libgif-dev libtiff5-dev libgnutls28-dev libncurses5-dev \
+     libjansson-dev libharfbuzz-dev libharfbuzz-bin
+./autogen.sh
+./configure --with-json --with-modules --with-harfbuzz --with-compress-install \
+            --with-threads --with-included-regex --with-zlib --without-sound \
+            --without-xpm --without-jpeg --without-tiff --without-gif --without-png \
+            --without-rsvg --without-imagemagick  --without-toolkit-scroll-bars \
+            --without-gpm --without-dbus --without-makeinfo --without-pop \
+            --without-mailutils --without-gsettings --without-pop --without-mailutils
+make
+sudo make install
