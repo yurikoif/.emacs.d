913d873633ceb51892a8b27b3cf8a4ff9025f26f
yurikoif <yurikoif@gmail.com>
2020-09-15 21:07:41 -0400

    1

---
+++ elpa/lsp-ui-20200915.529/lsp-ui-pkg.el
@@ -0,0 +1,16 @@
+(define-package "lsp-ui" "20200915.529" "UI modules for lsp-mode"
+  '((emacs "26.1")
+    (dash "2.14")
+    (dash-functional "1.2.0")
+    (lsp-mode "6.0")
+    (markdown-mode "2.3"))
+  :commit "2a5109620704818b5f1b9e67fc60a0aa5c25f721" :keywords
+  '("languages" "tools")
+  :authors
+  '(("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me"))
+  :maintainer
+  '("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me")
+  :url "https://github.com/emacs-lsp/lsp-ui")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
